9fd06f28987cb716dd708d887a287aabc16240b0
goldpu <yachesoup@naver.com>
2021-11-25 20:43:38 +0900

    update test

--- FirstTableauConnect.py
+++ FirstTableauConnect.py
@@ -1,3 +1,37 @@
 print("hello, Tableau visualization?")
-print("hello, Tableau visualization?")
-print("hello, Tableau visualization?")
+#https://www.kaggle.com/scratchpad/notebook8341c9020f/edit
+#Kaggle Data
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
+from plotly.offline  import download_plotlyjs,init_notebook_mode,plot, iplot
+import cufflinks as cf
+init_notebook_mode(connected = True)
+cf.go_offline()
+%matplotlib inline
+
+from plotly import tools
+import plotly.plotly as py
+from plotly.offline import init_notebook_mode, iplot
+init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+import plotly.figure_factory as ff
+import plotly.offline as offline
+# Squarify for treemaps
+import squarify
+# Random for well, random stuff
+import random
+# operator for sorting dictionaries
+import operator
+# For ignoring warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+
+
+df = pd.read_csv("../ufc_fighter_data.csv")
+df.head(2)
